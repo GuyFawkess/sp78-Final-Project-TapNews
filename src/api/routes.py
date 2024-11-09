fa1debcbfef677e557d4ef8d28cb5dc2b306dc25
nob86 <gustavoadallacroce@gmail.com>
2024-11-09 00:49:37 +0000

    route.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -11,6 +11,25 @@ api = Blueprint('api', __name__)
 # Allow CORS requests to this API
 CORS(api)
 
+from flask_jwt_extended import create_access_token
+
+# Crea una ruta para autenticar a los usuarios y devolver el token JWT
+# La función create_access_token() se utiliza para generar el JWT
+@api.route("/login", methods=["POST"])
+def create_token():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    # Consulta la base de datos por el nombre de usuario y la contraseña
+    user = User.query.filter_by(username=username, password=password).first()
+
+    if user is None:
+        # el usuario no se encontró en la base de datos
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    # Crea un nuevo token con el id de usuario dentro
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
