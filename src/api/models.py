8e3dcdcb2274f2c00752df4308e4761f764e108a
Patricia-Berges <patry37.zgz@gmail.com>
2024-11-17 14:34:00 +0000

    Fixed database problems, updated generation of user id

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     
-    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String(300), nullable=False)
