bf69663ac4ac3a6e1a1d29d12eb8b8ef1be2e4a4
nob86 <gustavoadallacroce@gmail.com>
2024-11-21 18:45:37 +0000

    cardNews.js Modal start

--- src/front/js/component/cardNews.js
+++ src/front/js/component/cardNews.js
@@ -1,6 +1,8 @@
 import React, {useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal'
+import Button from "react-bootstrap/Button"; 
 import "../../styles/card.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookmark, faHeart, faComment, faShare } from '@fortawesome/free-solid-svg-icons';
@@ -10,6 +12,8 @@ import { useAuth } from "../store/AuthContext";
 
 function CardNew() {
   const [description, setDescription] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [currentCommentNews, setCurrentCommentNews] = useState(null)
   const { store, actions } = useContext(Context);
   const { handleUserLogout } = useAuth();
 
@@ -59,6 +63,18 @@ function CardNew() {
     }
   }
 
+  // Abre el modal y establece la noticia seleccionada
+  const handleCommentClick = (news) => {
+    setCurrentCommentNews(news); // Guarda la noticia actual
+    setShowModal(true); // Muestra el modal
+  };
+
+  // Cierra el modal
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setCurrentCommentNews(null);
+  };
+
   return (
     <>
       {store.topnews.map((singleNew, index) => (
@@ -66,7 +82,7 @@ function CardNew() {
           <div className="actions d-flex flex-column" style={{backdropFilter: description ? 'brightness(30%)' : 'brightness(60%)'}}>
             <FontAwesomeIcon onClick={() => handleLike(singleNew.uuid)} size="2xl" icon={faHeart} style={likeStyle(singleNew.uuid)} className="like p-2" />
             <FontAwesomeIcon onClick={() => handleBookmark(singleNew.uuid)} size="2xl" icon={faBookmark} style={bookmarkStyle(singleNew.uuid)} className="save p-2" />
-            <FontAwesomeIcon size="2xl" icon={faComment} style={{color: "#FFFFFF"}} className="comment p-2" />
+            <FontAwesomeIcon size="2xl" icon={faComment} style={{color: "#FFFFFF"}} className="comment p-2" onClick={() => handleCommentClick(singleNew)}/>
             <FontAwesomeIcon size="2xl" icon={faShare} style={{color: "#FFFFFF"}} className="share p-2" />
           </div>
           <Card.Body style={{backdropFilter: description ? 'brightness(40%)' : 'brightness(70%)', marginTop: description ? '150%' : '170%'}} className="mycardbody">
@@ -77,6 +93,34 @@ function CardNew() {
           </Card.Body>
         </Card>
       ))}
+      
+      {/* modal para comentarios */}
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Comentarios</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {currentCommentNews ? (
+            <div>
+              <h5>{currentCommentNews.title}</h5>
+              <p>{currentCommentNews.description}</p>
+              <textarea
+                className="form-control"
+                placeholder="Escribe tu comentario..."
+                rows="3"
+              ></textarea>
+            </div>
+          ) : (
+            <p>Cargando datos...</p>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Cerrar
+          </Button>
+          <Button variant="primary">Enviar comentario</Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
