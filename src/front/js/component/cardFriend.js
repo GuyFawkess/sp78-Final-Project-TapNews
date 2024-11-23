e0bf3bb68e3c9f3234737ab31d503ba27f518eab
Alejandro Dayekh García <maestrodayekh@gmail.com>
2024-11-23 15:33:27 +0000

    cambio

--- src/front/js/component/cardFriend.js
+++ src/front/js/component/cardFriend.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser, faComments } from '@fortawesome/free-solid-svg-icons';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
-import TapNewsLogo from '../../../../public/1729329195515-removebg-preview.png';
+import TapNewsLogo from '../../../../public/tapnewslogo.png';
 
 const FriendCard = () => {
   const { store, actions } = useContext(Context);
